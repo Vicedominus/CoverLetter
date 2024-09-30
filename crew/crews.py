d099822fa76299e317f62eff38f2d6a9aa849fea
avice <avicedor@gmail.com>
2024-09-30 13:51:21 -0300

    changes in the agent to apply directly for jobs

--- crew/crews.py
+++ crew/crews.py
@@ -67,3 +67,37 @@ def get_agent_that_find_contribution(profile, job_post, symbol="<>"):
     response = crew.kickoff(inputs=inputs).raw
     print(response)
     return response.replace("my best complete final answer to the task.", "").replace(symbol, "")
+
+
+def get_cover_letter(name, client_name, job_title, profile, job_post, symbol="<>"):
+
+    agent = Agent(
+        role=job_title,
+        goal='Apply for jobs',
+        backstory="You are {name}, an experience and competent {job_title} looking for a job"
+    )
+
+    task = Task(
+        description="This is your profile: {profile}, and with that profile you have to apply for this job post: {job_post} posting"
+                    "by {client_name}",
+        agent=agent,
+        expected_output='Your application in its final version',
+    )
+
+    inputs = {
+        "name": name,
+        "job_title": job_title,
+        "profile": profile,
+        "job_post": job_post,
+        "client_name": client_name
+    }
+
+    crew = Crew(
+        agents=[agent],
+        tasks=[task],
+        verbose=True,
+    )
+
+    response = crew.kickoff(inputs=inputs).raw
+    print(response)
+    return response
