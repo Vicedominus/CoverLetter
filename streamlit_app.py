84f2f08b0a44404c6a70c224b8029f4a07dae0da
avice <avicedor@gmail.com>
2024-10-03 13:20:25 -0300

    update for introduce api key correctly

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ st.write("Upwork Cover Letter Generator")
 # Get info for the cover letter
 name_text = st.sidebar.text_input("Your Name", key="name")
 client_name_text = st.sidebar.text_input("Client's Name", key="client_name")
-job_title_text = st.sidebar.text_input("Your Job Title", key="job_title")
-profile_text = st.sidebar.text_input("Your Upwork Profile", key="profile", placeholder="Text")
+job_title_text = st.sidebar.text_input("Your Title", key="job_title")
+profile_text = st.sidebar.text_input("Your Profile", key="profile", placeholder="Text")
 job_post_text = st.sidebar.text_input("The Job Post", key="job_post", placeholder="Text")
-OPENAI_API_KEY = st.sidebar.text_input("The Job Post", key="job_post", placeholder="Text")
+OPENAI_API_KEY = st.sidebar.text_input("Your OpenAI api key", key="api_key", placeholder="Text")
 
 if (name_text and client_name_text and profile_text and job_post_text and OPENAI_API_KEY):
     # Asignar la clave de API directamente
-    openai.api_key = OPENAI_API_KEY
+    openai.api_key = st.session_state.api_key
     name = st.session_state.name
     client_name = st.session_state.client_name
     job_title = st.session_state.job_title
